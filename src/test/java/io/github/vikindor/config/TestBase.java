ddb212d8695a3be1833d89ed7d95756704a089c9
Vikindor <vikindor@protonmail.com>
2025-09-29 18:40:29 +0300

    test: add browser configs to TestBase

--- src/test/java/io/github/vikindor/config/TestBase.java
+++ src/test/java/io/github/vikindor/config/TestBase.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -13,11 +14,19 @@ public class TestBase {
     @BeforeAll
     static void setupConfig() {
         Configuration.baseUrl     = System.getProperty("baseUrl", "https://demoqa.com");
-//        Configuration.browser     = System.getProperty("browser", "chrome");
+        Configuration.browser     = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.headless    = Boolean.parseBoolean(System.getProperty("headless","false"));
+        Configuration.headless    = Boolean.parseBoolean(System.getProperty("headless","true"));
         Configuration.pageLoadStrategy = "eager";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        ChromeOptions opts = new ChromeOptions();
+        opts.addArguments("--no-sandbox");
+        opts.addArguments("--disable-dev-shm-usage");
+        opts.addArguments("--disable-gpu");
+        opts.addArguments("--remote-allow-origins=*");
+
+        Configuration.browserCapabilities = opts;
     }
 
     @AfterEach
