7ef033087736ed9052707d8315297cc59885545e
Vikindor <vikindor@protonmail.com>
2025-09-29 17:48:18 +0300

    test: add SelenideLogger

--- src/test/java/io/github/vikindor/config/TestBase.java
+++ src/test/java/io/github/vikindor/config/TestBase.java
@@ -1,6 +1,8 @@
 package io.github.vikindor.config;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
 
@@ -11,10 +13,11 @@ public class TestBase {
     @BeforeAll
     static void setupConfig() {
         Configuration.baseUrl     = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.browser     = System.getProperty("browser", "chrome");
+//        Configuration.browser     = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.headless    = Boolean.parseBoolean(System.getProperty("headless","false"));
         Configuration.pageLoadStrategy = "eager";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
